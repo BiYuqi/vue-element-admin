a715c9f569db185a771d5936bbec734f88046f99
biyuqi <biyuqiwan@163.com>
2018-03-29 00:22:48 +0800

    fiex some errorlog

--- src/errorLog.js
+++ src/errorLog.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
-import store from '@/store'
-
 // if (process.env.NODE_ENV) === 'production)'
 Vue.config.errorHandler = (err, vm, info) => {
   Vue.nextTick(() => {
-    store.commit('addErrorLog', {
+    vm.$store.commit('addErrorLog', {
       err,
       vm,
       info,
+      count: 0,
       url: err.stack.match(/((?:\w+\/)+\w+.vue):/m)[1]
     })
   })
